694849ba1b7d189bb602f421e8bee42a731e2881
Andrew Wason <rectalogic@rectalogic.com>
2023-02-18 16:24:55 -0500

    cleanup

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ import socket
 import struct
 import sys
 from enum import IntEnum
-from socketserver import BaseRequestHandler, TCPServer, ThreadingMixIn
+from socketserver import BaseRequestHandler, BaseServer, TCPServer, ThreadingMixIn
+import typing as ta
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -14,7 +15,6 @@ log = logging.getLogger(__name__)
 SOCKS_VERSION = 5
 RESERVED = 0
 FAILURE = 0xFF
-USERNAME_PASSWORD_VERSION = 1
 CONNECTION_TIMEOUT = 60 * 15 * 1000
 COPY_LOOP_BUFFER_SIZE = 4096
 BIND_PORT = (
@@ -25,8 +25,6 @@ BIND_PORT = (
 GREETING_SIZE = 2
 AUTH_METHOD_SIZE = 1
 VERSION_SIZE = 1
-ID_LEN_SIZE = 1
-PW_LEN_SIZE = 1
 CONN_NO_PORT_SIZE = 4
 CONN_PORT_SIZE = 2
 DOMAIN_SIZE = 1
@@ -66,7 +64,12 @@ class AddressDataType(IntEnum):
 class SOCKS5ProxyServer(ThreadingMixIn, TCPServer):
     """Initialise the socket server"""
 
-    def __init__(self, bind_address: str | None = None, port=1080, listen_ip="0.0.0.0"):
+    def __init__(
+        self,
+        bind_address: str | None = None,
+        port: int = 1080,
+        listen_ip: str = "0.0.0.0",
+    ):
         bind_addr = None
         if bind_address:
             # This should error out if invalid
@@ -96,8 +99,16 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
     in order to use binding for the request socket
     """
 
-    def __init__(self, bind_address: str | None = None):
+    def __init__(
+        self,
+        bind_address: str | None,
+        request: socket.socket,
+        client_address: ta.Any,
+        server: BaseServer,
+    ):
         self.bind_address = bind_address
+        self._address_type = AddressDataType.IPv4
+        super().__init__(request, client_address, server)
 
     def handle(self):
         log.info("Accepting connection from %s:%s" % self.client_address)
@@ -105,24 +116,28 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
         # Handle the greeting
         # Greeting header
         header = self._recv(
-            GREETING_SIZE, self._send_greeting_failure, AuthMethod.Invalid
+            GREETING_SIZE, lambda: self._send_greeting_failure(AuthMethod.Invalid)
         )
+        if header is None:
+            return
         version, nmethods = struct.unpack("!BB", header)
         # Only accept SOCKS5
         if version != SOCKS_VERSION:
             self._send_greeting_failure(AuthMethod.NoAuth)
+            return
         # We need at least one method
         if nmethods < 1:
             self._send_greeting_failure(AuthMethod.Invalid)
+            return
 
         # Get available methods
         methods = self._get_available_methods(nmethods)
         log.debug(f"Received methods {methods}")
 
-        # Accept only USERNAME/PASSWORD auth if we are asking for auth
-        # Accept only no auth if we are not asking for USERNAME/PASSWORD
+        # Accept only no auth
         if AuthMethod.NoAuth not in set(methods):
             self._send_greeting_failure(AuthMethod.Invalid)
+            return
 
         # Choose an authentication method and send it to the client
         self._send(struct.pack("!BB", SOCKS_VERSION, AuthMethod.NoAuth))
@@ -132,63 +147,80 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
 
         # Handle the request
         conn_buffer = self._recv(
-            CONN_NO_PORT_SIZE, self._send_failure, StatusCode.GeneralFailure
+            CONN_NO_PORT_SIZE, lambda: self._send_failure(StatusCode.GeneralFailure)
         )
+        if conn_buffer is None:
+            return
         version, cmd, rsv, address_type = struct.unpack("!BBBB", conn_buffer)
         # Do this so we can send an address_type in our errors
         # We don't want to send an invalid one back in an error so we will handle an invalid address type first
         # Microsocks just always sends IPv4 instead
-        if address_type in [
+        if address_type not in (
             AddressDataType.IPv4,
             AddressDataType.IPv6,
             AddressDataType.DomainName,
-        ]:
-            self._address_type = address_type
-        else:
+        ):
             self._send_failure(StatusCode.AddressTypeNotSupported)
+            return
+        self._address_type = address_type
 
         if version != SOCKS_VERSION:
             self._send_failure(StatusCode.GeneralFailure)
+            return
         if cmd != SocksCommand.CONNECT:  # We only support connect
             self._send_failure(StatusCode.CommandNotSupported)
+            return
         if rsv != RESERVED:  # Malformed packet
             self._send_failure(StatusCode.GeneralFailure)
+            return
 
-        log.debug(f"Handling request with address type: {address_type}")
+        log.debug(f"Handling request with address type: {self._address_type}")
 
         if (
-            address_type == AddressDataType.IPv4 or address_type == AddressDataType.IPv6
+            self._address_type == AddressDataType.IPv4
+            or self._address_type == AddressDataType.IPv6
         ):  # IPv4 or IPv6
             address_family = (
                 socket.AF_INET
-                if address_type == AddressDataType.IPv4
+                if self._address_type == AddressDataType.IPv4
                 else socket.AF_INET6
             )
-            minlen = 4 if address_type == AddressDataType.IPv4 else 16
+            minlen = 4 if self._address_type == AddressDataType.IPv4 else 16
             raw = self._recv(
-                minlen, self._send_failure, StatusCode.GeneralFailure
+                minlen, lambda: self._send_failure(StatusCode.GeneralFailure)
             )  # Raw IP address bytes
-
+            if raw is None:
+                return
             # Convert the IP address from binary to text
             try:
                 address = socket.inet_ntop(address_family, raw)
             except Exception:
                 log.exception(f"Could not convert packed IP {raw} to string")
                 self._send_failure(StatusCode.GeneralFailure)
-        elif address_type == AddressDataType.DomainName:  # Domain name
+                return
+        elif self._address_type == AddressDataType.DomainName:  # Domain name
             domain_buffer = self._recv(
-                DOMAIN_SIZE, self._send_failure, StatusCode.GeneralFailure
+                DOMAIN_SIZE, lambda: self._send_failure(StatusCode.GeneralFailure)
             )
+            if domain_buffer is None:
+                return
             domain_length = domain_buffer[0]
             if domain_length > 255:  # Invalid
                 self._send_failure(StatusCode.GeneralFailure)
             address = self._recv(
-                domain_length, self._send_failure, StatusCode.GeneralFailure
+                domain_length, lambda: self._send_failure(StatusCode.GeneralFailure)
             )
+            if address is None:
+                return
+        else:
+            self._send_failure(StatusCode.AddressTypeNotSupported)
+            return
 
         port_buffer = self._recv(
-            CONN_PORT_SIZE, self._send_failure, StatusCode.GeneralFailure
+            CONN_PORT_SIZE, lambda: self._send_failure(StatusCode.GeneralFailure)
         )
+        if port_buffer is None:
+            return
         port = struct.unpack("!H", port_buffer)[0]
 
         # Translate our address and port into data from which we can create a socket connection
@@ -208,6 +240,7 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
         ):  # There's no suitable errorcode in RFC1928 for DNS lookup failure
             log.exception("DNS lookup")
             self._send_failure(StatusCode.GeneralFailure)
+            return
 
         af, socktype, proto, _, sa = remote_info
 
@@ -217,7 +250,7 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
             self._remote = socket.socket(af, socktype, proto)
             # Bind it to an IP
             if self.bind_address:
-                self._remote.bind(bind_address)
+                self._remote.bind(self.bind_address)
             self._remote.connect(sa)
             bind_address = self._remote.getsockname()
             log.info(f"Connected to {address} {port}")
@@ -230,6 +263,7 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
             log.exception("bind failed")
             # TO-DO: Get the actual failure code instead of giving ConnRefused each time
             self._send_failure(StatusCode.ConnRefused)
+            return
 
         # TO-DO: Are the BND.ADDR and BND.PORT returned correct values?
         self._send(
@@ -248,29 +282,28 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
         self._copy_loop(self.request, self._remote)
         self._exit(True)
 
-    def _send(self, data):
+    def _send(self, data: bytes):
         """Convenience method to send bytes to a client"""
         return self.request.sendall(data)
 
-    def _recv(self, bufsize, failure_method=False, code=False):
+    def _recv(self, bufsize, failure_method: ta.Callable | None = None) -> bytes | None:
         """Convenience method to receive bytes from a client
         If bufsize is less than the size of the data received, then failure_method is called with code as a parameter and kills the thread
         """
         buf = self.request.recv(bufsize)
         if len(buf) < bufsize:
-            if failure_method and code:
-                failure_method(code)
-            elif failure_method:
+            if failure_method:
                 failure_method()
             else:
                 self._exit()  # Kill thread if we aren't calling the failure methods (they already do this)
+            return None
         return buf
 
     def _shutdown_client(self):
         """Convenience method to shutdown and close the connection with a client"""
         self.server.shutdown_request(self.request)
 
-    def _exit(self, dontExit=False):
+    def _exit(self, dontExit: bool = False):
         """Convenience method to exit the thread and cleanup any connections"""
         self._shutdown_client()
         if hasattr(self, "_remote"):
@@ -279,39 +312,33 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
         if not dontExit:
             sys.exit()
 
-    def _get_available_methods(self, n):
+    def _get_available_methods(self, n: int) -> list[int]:
         """Receive the methods a client supported and return them as a list"""
-        methods = []
+        methods: list[int] = []
         for i in range(n):
-            methods.append(
-                ord(
-                    self._recv(
-                        AUTH_METHOD_SIZE,
-                        self._send_greeting_failure,
-                        AuthMethod.Invalid,
-                    )
-                )
+            method = self._recv(
+                AUTH_METHOD_SIZE,
+                lambda: self._send_greeting_failure(AuthMethod.Invalid),
             )
+            if method is not None:
+                methods.append(ord(method))
         return methods
 
-    def _send_greeting_failure(self, code):
+    def _send_greeting_failure(self, code: AuthMethod):
         """Convinence method to send a failure message to a client in the greeting stage"""
         self._send(struct.pack("!BB", SOCKS_VERSION, code))
         self._exit()
 
-    def _send_failure(self, code):
+    def _send_failure(self, code: StatusCode):
         """Convinence method to send a failure message to a client in the socket stage"""
-        address_type = (
-            self._address_type
-            if hasattr(self, "_address_type")
-            else AddressDataType.IPv4
-        )
         self._send(
-            struct.pack("!BBBBIH", SOCKS_VERSION, code, RESERVED, address_type, 0, 0)
+            struct.pack(
+                "!BBBBIH", SOCKS_VERSION, code, RESERVED, self._address_type, 0, 0
+            )
         )
         self._exit()
 
-    def _copy_loop(self, client, remote):
+    def _copy_loop(self, client: socket.socket, remote: socket.socket):
         """Waits for network activity and forwards it to the other connection"""
         while True:
             # Wait until client or remote is available for read
@@ -324,6 +351,7 @@ class SOCKS5ProxyHandler(BaseRequestHandler):
             # Kill inactive/unused connections
             if not r and not w and not e:
                 self._send_failure(StatusCode.TTLExpired)
+                return
 
             for sock in r:
                 try:
